7ea44765fdaa5e66368204dbcef207b8c610460a
mdrum <mdrum@psi.edu>
2018-04-12 14:29:46 -0700

    this is better

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
+// node modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const readSync = require('read-file-relative').readSync;
 const xml2js = require('xml2js');
-const gulp = require('gulp');
-const xslt = require('gulp-xslt');
-const through = require('through');
-// const async = require('async');
+const libxslt = require('libxslt');
 
-const parseString = xml2js.parseString;
-const builder = new xml2js.Builder();
+// xml/js transformers
+const parseXmlString = xml2js.parseString;
+const xmlBuilder = new xml2js.Builder();
 
+// express server setup
 const app = express();
 app.use(bodyParser.text({ type: 'application/xml' }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
+app.listen(3001);
 
-app.get('/particles', function(req, res) {
-    
-    let data = readSync('/ldd-particle.xml');
+// preload xml stylesheets
+const htmlxslt = readSync('/IngestLddView.xsl');
 
-    parseString(data, function(err, result) {
-        if (err && err.length > 0) {
-            res.writeHead(500);
-            res.send(err);
-        } else {
-            res.json(result);
-        }
-    })
-});
-
-app.get('/imaging', function(req, res) {
-    
-    let data = readSync('/PDS4_IMG_IngestLDD_1900.xml');
-
-    parseString(data, function(err, result) {
-        if (err && err.length > 0) {
-            res.writeHead(500);
-            res.send(err);
-        } else {
-            res.json(result);
-        }
-    })
-});
-
-app.post('/xmltojson', function (req, res) {
+app.post('/xml/to/json', function (req, res) {
     let xml = req.body
 
-    parseString(xml, function(err, result) {
+    parseXmlString(xml, function(err, result) {
         if (err && err.length > 0) {
             res.writeHead(500);
             res.send(err);
@@ -55,39 +32,28 @@ app.post('/xmltojson', function (req, res) {
     })
 });
 
-app.post('/jsontoxml', function (req, res) {
+app.post('/json/to/xml', function (req, res) {
     let object = req.body
-
-    let xml = builder.buildObject(object);
+    let xml = xmlBuilder.buildObject(object);
     res.send(xml);
 });
 
-function sendTo(res) {
-    return through(
-        function write(data) {
-            res.write(data.contents);
-        },
-        function end() {
-            res.end();
+app.post('/xml/to/html', function(req, res) {
+    let xml = req.body;
+    
+    libxslt.parse(htmlxslt, function(err, stylesheet) {
+        if (err && err.length > 0) {
+            res.writeHead(500);
+            res.send(err);
+        } else {   
+            stylesheet.apply(xml, function(err, result) {
+                if (err && err.length > 0) {
+                    res.writeHead(500);
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            })
         }
-    )
-}
-
-app.get('/particles-html', function(req, res) {
-
-    gulp.src('./ldd-particle.xml')
-        .pipe(xslt('./IngestLddView.xsl'))
-        .pipe(sendTo(res));
+    });
 });
-
-app.get('/particles-dot', function(req, res) {
-
-    gulp.src('./ldd-particle.xml')
-        .pipe(xslt('./IngestLddDot.xsl'))
-        .pipe(sendTo(res));
-});
-
-// serve anything in the public directory directly
-app.use(express.static('public'));
-
-app.listen(3001);
