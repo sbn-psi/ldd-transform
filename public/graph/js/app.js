1cc300299b6716b3db75423b1f1f2f9d4bbf3812
Conor Kingston <cmkingston21@me.com>
2018-08-30 16:49:06 -0700

    new angular app

--- public/graph/js/app.js
+++ public/graph/js/app.js
@@ -1,12 +1,5 @@
 let app = angular.module('LD3', []);
 
-app.controller('ld3Controller', ['$scope', function($scope) {
-    // initialize application state
-    $scope.ld3 = {
-        legendIsVisible: true,
-    };
-}]);
-
 app
     .directive('ld3Toolbox', () => {
         return {
@@ -18,8 +11,50 @@ app
             templateUrl: './partials/angular/ld3-toolbar.html'
         }
     })
+    .directive('ld3ActiveNode', () => {
+        return {
+            templateUrl: './partials/angular/ld3-active-node.html',
+            scope: {
+                node: '='
+            },
+            controller: function($scope) {
+                $scope.showDetails = true;
+            }
+        }
+    })
+    .directive('ld3ActiveNodeChildren', () => {
+        return {
+            templateUrl: './partials/angular/ld3-active-node-children.html',
+            scope: {
+                children: '='
+            }
+        }
+    })
+    .directive('ld3ActiveNodeParents', () => {
+        return {
+            templateUrl: './partials/angular/ld3-active-node-parents.html',
+            scope: {
+                parents: '='
+            }
+        }
+    })
     .directive('ld3Modal', () => {
         return {
             templateUrl: './partials/angular/ld3-modal.html'
+        }
+    })
+    .directive('ld3NodeForm', () => {
+        return {
+            templateUrl: './partials/angular/ld3-node-form.html'
+        }
+    })
+    .directive('ld3NewClass', () => {
+        return {
+            templateUrl: './partials/angular/ld3-new-class.html'
+        }
+    })
+    .directive('ld3NewAttribute', () => {
+        return {
+            templateUrl: './partials/angular/ld3-new-attribute.html'
         }
     })
