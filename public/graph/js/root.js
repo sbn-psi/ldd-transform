4a7bf67cd4678327b3260030bedd88e966f4c2c1
Conor Kingston <cmkingston21@me.com>
2018-09-07 15:29:34 -0700

    removed unused templates

--- public/graph/js/root.js
+++ public/graph/js/root.js
@@ -1,35 +1,9 @@
-const _root = {
-    "Ingest_LDD": {
-        "$": {
-            "xmlns": "http://pds.nasa.gov/pds4/pds/v1",
-            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-            "xsi:schemaLocation": "http://pds.nasa.gov/pds4/pds/v1 \n        http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1A00.xsd "
-        },
-        "name": ["New Ingest_LDD"],
-        "ldd_version_id": ["1.0.0.0"],
-        "full_name": ["Your Name"],
-        "steward_id": [""],
-        "namespace_id": [""],
-        "comment": [""],
-        "last_modification_date_time": [""],
-        "DD_Attribute": [],
-        "DD_Class": [{
-            "name": ["Click to Edit"],
-            "version_id": [""],
-            "local_identifier": ["Click to Edit"],
-            "submitter_name": [""],
-            "definition": [""],
-            "DD_Association": []
-        }]
-    }
-};
-
 const _template = {
     "Ingest_LDD": {
         "$": {
             "xmlns": "http://pds.nasa.gov/pds4/pds/v1",
             "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-            "xsi:schemaLocation": "http://pds.nasa.gov/pds4/pds/v1 http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1900.xsd"
+            "xsi:schemaLocation": "http://pds.nasa.gov/pds4/pds/v1 http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1A00.xsd"
         },
         "name": ["Template IngestLDD"],
         "ldd_version_id": ["1.0.0.0"],
@@ -65,87 +39,3 @@ const _template = {
         }]
     }
 };
-
-const _testRoot = {
-    "Ingest_LDD": {
-        "$": {
-            "xmlns": "http://pds.nasa.gov/pds4/pds/v1",
-            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-            "xsi:schemaLocation": "http://pds.nasa.gov/pds4/pds/v1 \n        http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1A00.xsd "
-        },
-        "name": ["Test Name"],
-        "ldd_version_id": ["1.0.0.0"],
-        "full_name": ["Johnny Appleseed"],
-        "steward_id": ["sbn"],
-        "namespace_id": ["test"],
-        "comment": ["This is a test comment."],
-        "last_modification_date_time": [""],
-        "DD_Attribute": [],
-        "DD_Class": [{
-            "name": ["Root"],
-            "version_id": ["0.0.0.0"],
-            "local_identifier": ["sample.Root"],
-            "submitter_name": ["Johnny Appleseed"],
-            "definition": ["The definition of the element."],
-            "DD_Association": []
-        }]
-    }
-}
-
-const _devRoot = {
-    "Ingest_LDD": {
-        "$": {
-            "xmlns": "http://pds.nasa.gov/pds4/pds/v1",
-            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-            "xsi:schemaLocation": "http://pds.nasa.gov/pds4/pds/v1 \n        http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1A00.xsd "
-        },
-        "name": ["Test Name"],
-        "ldd_version_id": ["1.0.0.0"],
-        "full_name": ["Johnny Appleseed"],
-        "steward_id": ["sbn"],
-        "namespace_id": ["test"],
-        "comment": ["This is a test comment."],
-        "last_modification_date_time": [""],
-        "DD_Attribute": [{
-            "name": ["attr"],
-            "version_id": ["1.0"],
-            "identifier_reference": ["test.attr"],
-            "submitter_name": ["conor"],
-            "definition": ["def"],
-            "nillable_flag": ["true"]
-        }, {
-            "name": ["attr2"],
-            "version_id": ["1.0"],
-            "identifier_reference": ["test.attr2"],
-            "submitter_name": ["conor"],
-            "definition": ["def"],
-            "nillable_flag": ["true"]
-        }],
-        "DD_Class": [{
-            "name": ["Root"],
-            "version_id": ["0.0.0.0"],
-            "local_identifier": ["sample.Root"],
-            "submitter_name": ["Johnny Appleseed"],
-            "definition": ["The definition of the element."],
-            "DD_Association": [{
-                "name": ["attr"],
-                "definition": ["def"],
-                "identifier_reference": ["test.attr"],
-                "version_id": ["1.0"],
-                "submitter_name": ["conor"],
-                "reference_type": ["attribute_of"],
-                "minimum_occurrences": ["1"],
-                "maximum_occurrences": ["1"]
-            }, {
-                "name": ["attr2"],
-                "definition": ["def"],
-                "identifier_reference": ["test.attr2"],
-                "version_id": ["1.0"],
-                "submitter_name": ["conor"],
-                "reference_type": ["attribute_of"],
-                "minimum_occurrences": ["1"],
-                "maximum_occurrences": ["1"]
-            }]
-        }]
-    }
-}
