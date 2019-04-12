92aaa8b4773060ad3dee5c81a11e39d4b175602e
Conor Kingston <cmkingston21@me.com>
2019-04-12 12:38:03 -0700

    modal is single shared object

--- public/graph/js/services.Modal.js
+++ public/graph/js/services.Modal.js
@@ -1,25 +1,17 @@
 app.factory('Modal', function($rootScope) {
-    const Modal = function(type) {
-        if (!type) type = null;
-        return {
-            isVisible: false,
-            type: type,
-            show: function(type) {
-                this.isVisible = true;
-                this.type = type;
-                $rootScope.$broadcast('modal-show');
-            },
-            hide: function() {
-                this.isVisible = false;
-                this.type = null;
-                $rootScope.$broadcast('modal-hide');
-            }
-        };
-    };
-
-    return {
-        new: function(type) {
-            return new Modal(type);
+    let Modal = {
+        isVisible: false,
+        type: null,
+        show: function(type) {
+            this.isVisible = true;
+            this.type = type;
+            $rootScope.$broadcast('modal-show');
+        },
+        hide: function() {
+            this.isVisible = false;
+            this.type = null;
+            $rootScope.$broadcast('modal-hide');
         }
-    }
+    };
+    return Modal;
 });
