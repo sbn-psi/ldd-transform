ea3b8dfcc8edf85d8f5ef6f7d7336521c301eac1
Conor Kingston <cmkingston21@me.com>
2019-04-18 14:21:43 -0700

    a user can define a new class element

--- tests/specs/class.spec.js
+++ tests/specs/class.spec.js
@@ -24,11 +24,32 @@ const select = {
             done();
         });
     }
-}
+};
 
 describe('A user', () => {
     it('can define a new CLASS element', done => {
-        select.classNode(done);
+        select.classNode(() => {
+            // open add class form
+            element(by.id('add-class')).click();
+            
+            // fill form values
+            // name
+            element(by.css('[ng-model="pds4Keyword"]#name')).sendKeys('Test_Class');
+            // definition
+            element(by.css('[ng-model="pds4Keyword"]#definition')).sendKeys('This is a test definition for a test class.');
+            // maximum occurrences
+            element(by.css('[ng-model="pds4Keyword"]#maximum_occurrences')).sendKeys('3');
+            
+            // submit form
+            element(by.css('[ng-click="form.addClass()"]')).click();
+            
+            // wait for animations
+            browser.sleep(2000);
+            
+            // test that new class is present in D3 tree
+            expect(element(by.id('grns-Test_Class')).isDisplayed()).toBeTruthy();
+            done();
+        });
     });
     
     xit('can modify an existing CLASS element', () => {
