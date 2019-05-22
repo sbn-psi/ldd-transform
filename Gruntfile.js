e9841e74390cfbfa919a9060beaa95632f3a12b3
Conor Kingston <cmkingston21@me.com>
2019-05-22 11:15:41 -0700

    no grunt deployment tasks; docker responsible for all deployment tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,5 @@
 module.exports = function(grunt) {
-
-    var files = [{
-        expand: true,
-        src: [
-            'public/**/*',
-            '!**/legend/*',
-            '*',
-            '!node_modules',
-            '!.git'
-        ],
-        dest: 'build/',
-        options: {
-            noProcess: '**/*.png'
-        }
-    }];
-
     grunt.initConfig({
-        clean: ['build'],
-        copy: {
-            dev: {
-                files: files
-            },
-            production: {
-                files: files,
-                options: {
-                    process: function(content,srcpath) {
-                        content = content
-                            .replace('<base href="/">','<base href="/ld3/">')
-                            .replace('<base href="/graph/">','<base href="/ld3/graph/">')
-
-                        return content;
-                    }
-                }
-            },
-            images: {
-                expand: true,
-                flatten: true,
-                src: [
-                    'public/graph/img/legend.png',
-                    'public/graph/img/logo.png',
-                    'public/graph/img/logo_no_title.png'
-                ],
-                dest: 'build/public/graph/img/'
-            }
-        },
         protractor: {
             options: {
                 configFile: './tests/protractor.conf.js',
@@ -67,12 +23,8 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-protractor-runner');
 
-    grunt.registerTask('deploy:dev', ['clean', 'copy:dev', 'copy:images']);
-    grunt.registerTask('deploy:production', ['clean', 'copy:production', 'copy:images']);
     grunt.registerTask('test', ['protractor:runAll']);
     grunt.registerTask('test:class', ['protractor:class']);
 };
